d3c2d85c2b5da09cd680d38ff546b9ea75275a57
Alvin Chow <alvinchow86@gmail.com>
2015-03-05 15:20:20 -0800

    cleanup

--- rohm/fields.py
+++ rohm/fields.py
@@ -2,16 +2,14 @@ class BaseField(object):
     # _field_name = None  # placeholder
 
     def __init__(self, *args, **kwargs):
-        print 'field init', args, kwargs
+        pass
+        # print 'field init', args, kwargs
 
     def __get__(self, instance, owner):
-        print 'get', instance, owner
         val = instance._data.get(self._field_name, None)
         return val
-        # return 123
 
     def __set__(self, instance, value):
-        print 'set', instance, value
         instance._data[self._field_name] = value
 
 
