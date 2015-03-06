1ecf1aa161dad21cb6f45bfed7471a8cea908b8f
Alvin Chow <alvinchow86@gmail.com>
2015-03-05 16:05:14 -0800

    auto primary key

--- rohm/fields.py
+++ rohm/fields.py
@@ -1,16 +1,17 @@
 class BaseField(object):
     # _field_name = None  # placeholder
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, primary_key=False, required=False, allow_none=True, *args, **kwargs):
+        self.is_primary_key = primary_key
+        self.required = required
         # print 'field init', args, kwargs
 
     def __get__(self, instance, owner):
-        val = instance._data.get(self._field_name, None)
+        val = instance._data.get(self.field_name, None)
         return val
 
     def __set__(self, instance, value):
-        instance._data[self._field_name] = value
+        instance._data[self.field_name] = value
 
 
 class IntegerField(BaseField):
--- rohm/models.py
+++ rohm/models.py
@@ -1,28 +1,64 @@
 import six
 
-from rohm.fields import BaseField
+from rohm.fields import BaseField, IntegerField
+
+from rohm.connection import get_connection
+
+
+conn = get_connection()
 
 
 class ModelMetaclass(type):
-    # def __new__(cls, name, bases, attrs):
-    #     print cls, name, bases, attrs
-    #     return super(ModelMetaclass, cls).__new__(cls, name, bases, attrs)
+    def __new__(meta, name, bases, attrs):
+
+        primary_key = None
+        for key, val in attrs.items():
+            if isinstance(val, BaseField) and val.is_primary_key:
+                primary_key = key
+                break
+
+        if primary_key is None:
+            id_field = IntegerField(primary_key=True)
+            primary_key = 'id'
+            attrs[primary_key] = id_field
+
+        attrs['_primary_key'] = primary_key
+        return super(ModelMetaclass, meta).__new__(meta, name, bases, attrs)
 
     def __init__(cls, name, bases, attrs):
 
         super(ModelMetaclass, cls).__init__(name, bases, attrs)
 
         cls._fields = {}
+        # cls._primary_key = None
         for key, val in attrs.items():
             if isinstance(val, BaseField):
                 # let field know its name!
-                val._field_name = key
-                cls._fields[key] = val
+                field = val
+                val.field_name = field
+                cls._fields[key] = field
+
+                # if field.is_primary_key:
+                #     cls._primary_key = key
 
 
 class Model(six.with_metaclass(ModelMetaclass)):
+
     def __init__(self, **kwargs):
         self._data = {}
+
         for key, val in kwargs.items():
             if key in self._fields:
                 setattr(self, key, val)
+
+    def get(self):
+        # get from redis
+        pass
+
+    def get_or_create(self):
+        # create in Redis if it doesn't exist
+        pass
+
+    def save(self):
+        # save to redis fool!
+        pass
